801ff99986f953476884a56d9627ac4bcb1d9f78
DESKTOP-KR3UD5D\NfryCreator <54572905@qq.com>
2023-05-10 14:16:43 +0800

    数据类型、变量声明

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,51 @@
+/*
+* 基础使用
+*
+
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is Fun!");
+// if (js === "amazing") alert("JavaScript is Fun!");
 console.log(40+12+9-5);
+
+console.log("Bob");
+console.log(23);
+
+let firstName = "Bob";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let b3a = 7;
+
+// console.log(PI);
+
+let myFirstJob = "programmer";
+let myCurrentJob = "manager";
+
+console.log(myFirstJob+" "+myCurrentJob);
+
+*
+* * */
+/*
+* 数据类型
+console.log(false);
+let judge = true;
+console.log(judge);
+
+console.log(typeof 12);
+console.log(typeof true);
+console.log(typeof "Bob");
+console.log(typeof judge);
+
+judge = "Bob's farther.";
+console.log(typeof judge);
+
+
+console.log(typeof undefined);
+console.log(typeof null+" bug");
+console.log(undefined === null);
+* */
+let age = 30;
+age = 31;
+const birthdayear = 2.2;
+birthdayear = 3;
