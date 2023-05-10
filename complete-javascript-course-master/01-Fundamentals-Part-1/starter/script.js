5c084c4b9110ad52e0b91f89182aca31074cf335
DESKTOP-KR3UD5D\NfryCreator <54572905@qq.com>
2023-05-10 15:17:00 +0800

    算术运算符、比较运算符、优先级

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -45,7 +45,43 @@ console.log(typeof undefined);
 console.log(typeof null+" bug");
 console.log(undefined === null);
 * */
+/*
+* 声明类型
+*
 let age = 30;
 age = 31;
 const birthdayear = 2.2;
-birthdayear = 3;
+birthdayear = 3; //不允许的操作
+* */
+/*
+* 运算符
+const year = 2023;
+const ageJonas = year - 1999;
+const ageBob = year - 1992;
+console.log(ageJonas, ageBob);
+console.log(ageJonas * 2, ageBob / 2, 2 ** 3);
+
+const firstName = "Bob";
+const LastName = "John";
+console.log(firstName + " " + LastName);
+
+let x = 10 + 5;
+let y = 10 - 5;
+x += 10;
+console.log(x)
+x -= 5;
+console.log(x)
+x *= 5;
+console.log(x)
+x++;
+console.log(x)
+x--;
+console.log(x)
+
+console.log(ageJonas > ageBob);
+
+let z, w;
+z = w = 20 - 13;
+console.log(z,w)
+* */
+
