a146faa389c8f84fc444050bb4b7c43258bb00a0
DESKTOP-KR3UD5D\NfryCreator <54572905@qq.com>
2023-05-24 09:59:43 +0800

    寻找最值、合并数组、问题分析、问题搜索

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,7 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-const x = 12;
+/*const x = 12;
 
 if (x === 12) {
   console.log("It's right.");
@@ -9,4 +9,71 @@ if (x === 12) {
 
 const calcAge = birthday => 2037 - birthday;
 
-console.log(`I'm here!`);
+console.log(`I'm here!`);*/
+
+const array = [2,12,13,5,9];
+const array2 = [2,12,13,"error",5,9,17,5,3,6];
+
+function getMaxValue(array){
+  let maxValue = array[0];
+
+  for(let i =1;i<array.length;i++){
+    if (maxValue<array[i]){
+      maxValue = array[i];
+    }
+  }
+
+  return maxValue;
+}
+
+function getMaxAndMinValue(array){
+  let maxValue = array[0];
+  let minValue = array[0];
+
+  for(let i =1;i<array.length;i++){
+    if (maxValue<array[i]){
+      maxValue = array[i];
+    }
+    if (minValue>array[i]){
+      minValue = array[i];
+    }
+  }
+
+  return {
+    maxValue,
+    minValue
+  };
+}
+
+function getMaxAndMinValue_judgement(array){
+  let maxValue = array[0];
+  let minValue = array[0];
+
+  for(let i =1;i<array.length;i++){
+    if(typeof array[i] === "string") continue;
+    if (maxValue<array[i]){
+      maxValue = array[i];
+    }
+    if (minValue>array[i]){
+      minValue = array[i];
+    }
+  }
+
+  return {
+    maxValue,
+    minValue
+  };
+}
+
+function mergeArray (array1,array2){
+  return array1.concat(array2);
+}
+console.log(getMaxValue(array));
+
+console.log(getMaxAndMinValue(array));
+
+console.log(getMaxAndMinValue_judgement(array2));
+
+console.log(array instanceof Array,array+array2);
+
+console.log(mergeArray(array,array2));
