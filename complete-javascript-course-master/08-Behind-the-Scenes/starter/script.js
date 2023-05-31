2bac3961c81f3e9e9375f23cadafff4af0cc8984
DESKTOP-KR3UD5D\NfryCreator <54572905@qq.com>
2023-05-31 15:41:22 +0800

    作用域与作用域链

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+const a = "aaa";
+
+function first(){
+    const b = "bbb";
+    function second(){
+        const c = "ccc";
+        function third(){
+            const d = "ddd";
+            console.log(d,c,b,a);
+        }
+        third();
+    }
+    second();
+}
+first();
