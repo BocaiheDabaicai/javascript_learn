7a39438dca0527a72c3958aa57651f285e0755c6
DESKTOP-KR3UD5D\NfryCreator <54572905@qq.com>
2023-06-08 15:59:01 +0800

    挑战一 足球比赛，数组循环for of，增强模板文字

--- README.md
+++ README.md
@@ -280,3 +280,140 @@ rest2.owner &&= '<Apple>';
 console.log(rest1);
 console.log(rest2);
 ```
+
+#### 挑战一 足球比赛
+
+```js
+// Coding Challenge #1
+
+/*
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
+
+
+// 完成解构
+const [player1,player2] = game.players;
+console.log(player1,player2);
+
+// 数组解构
+const [goalKeeper,...fieldPlayers] = player1;
+console.log(goalKeeper,fieldPlayers);
+
+// 数组合并
+const allPlayers = [...player1,...player2];
+console.log(allPlayers);
+
+// 数组合并2
+const players1Final = [...player1,...['Thiago', 'Coutinho', 'Perisic']];
+console.log(players1Final);
+
+// 数组深度解构并重命名
+const {odds:{team1,x:draw,team2}} = game;
+console.log(team1,draw,team2);
+
+// 函数参数解构
+function printGoals(...players){
+    console.log(`${players.length} goals are scored.`);
+}
+
+printGoals(...game.scored);
+
+// 逻辑且判断为真，则执行最后一句语言
+team1 < team2 && console.log(`team1 will be more likely to win.`);
+team1 > team2 && console.log(`team2 will be more likely to win.`);
+```
+
+---
+
+#### 8.循环结构(for of)
+
+本意：更加方便的获取到数组的每一个元素
+
+代码如下:
+
+```js
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// 数组迭代
+for (const item of menu) {
+    console.log(item);
+}
+
+// 数组迭代，并生成下标
+for (const item of menu.entries()) {
+    console.log(item);
+}
+
+// 数组迭代，生成下标，并解构获取
+for( const [index,item] of menu.entries()){
+    console.log(`${index}: ${item}`);
+}
+```
+
+#### 9.增强模板文字
+
+- 声明与赋值可以用声明变量名进行表达`{name}`
+
+- 对象中的函数可以不用可以表达`funtion`
+
+- 子对象可以使用数组符号进行赋值`[array[0]]:{}`
+
+示例如下:
+
+```js
+
+```
--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -11,7 +11,7 @@ const restaurant = {
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-    numGuests:23,
+    numGuests: 23,
 
     openingHours: {
         thu: {
@@ -230,7 +230,7 @@ console.log(`a=0`);
 a = 0;
 console.log(a || 10);
 console.log(a ?? 10);*/
-
+/* 逻辑运算符赋值
 const rest1 = {
     name:'Bob',
     numGuests:12
@@ -253,4 +253,141 @@ rest1.owner &&= '<Apple>';
 rest2.owner &&= '<Apple>';
 
 console.log(rest1);
-console.log(rest2);
+console.log(rest2);*/
+/*
+///////////////////////////////////////
+// Coding Challenge #1
+
+/!*
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*!/
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
+
+
+
+const [player1,player2] = game.players;
+console.log(player1,player2);
+
+const [goalKeeper,...fieldPlayers] = player1;
+console.log(goalKeeper,fieldPlayers);
+
+const allPlayers = [...player1,...player2];
+console.log(allPlayers);
+
+const players1Final = [...player1,...['Thiago', 'Coutinho', 'Perisic']];
+console.log(players1Final);
+
+const {odds:{team1,x:draw,team2}} = game;
+console.log(team1,draw,team2);
+
+function printGoals(...players){
+    console.log(`${players.length} goals are scored.`);
+}
+
+printGoals(...game.scored);
+
+team1 < team2 && console.log(`team1 will be more likely to win.`);
+team1 > team2 && console.log(`team2 will be more likely to win.`);*/
+/*循环结构(for of)
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) {
+    console.log(item);
+}
+
+for (const item of menu.entries()) {
+    console.log(item);
+}
+
+for( const [index,item] of menu.entries()){
+    console.log(`${index}: ${item}`);
+}*/
+/* 增强模板文字
+const name = {
+    man: ['Bob', 'Jack'],
+    woman: ['Jassiy', 'Mooly']
+}
+
+const time = [10, 12, 18];
+
+const object = {
+    name,
+    getName() {
+        console.log(...this.name.man);
+        console.log(...this.name.woman);
+    },
+    thing: {
+        [time[0]]: {
+            job: 'writter',
+            money: 12
+        },
+        [time[1]]: {
+            job: 'painter',
+            money: 5
+        },
+        [time[2]]: {
+            job: 'guard',
+            money: 24
+        },
+        [`job-${time[2]}`]: {
+            job: 'sleep',
+            money: 500
+        },
+    }
+}
+
+console.log(object);
+object.getName();*/
