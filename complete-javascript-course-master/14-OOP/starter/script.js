81bad1ce4df627c9be5e29f8628a555782a614e0
DESKTOP-KR3UD5D\NfryCreator <54572905@qq.com>
2023-07-28 10:43:10 +0800

    第十章 面向对象编程，创建类实例对象

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const Person = function (firstName,birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+}
+
+const Bob = new Person('Bob',2003);
+const Jack = new Person('Jack',2001);
+
+console.log(Bob)
+console.log(Jack)
+
+
