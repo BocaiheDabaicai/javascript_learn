257ca2b98766885ba6b441950fc4da9c39d10779
DESKTOP-KR3UD5D\NfryCreator <54572905@qq.com>
2023-08-04 14:49:46 +0800

    获取地图坐标

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -10,3 +10,33 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if(navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            const {latitude,longitude} = position.coords
+            console.log(latitude,longitude)
+            console.log(`https://www.google.com/maps/@${latitude},${longitude},11z?entry=ttu`)
+        },
+        function () {
+            alert(`Could not get your position`)
+        }
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
