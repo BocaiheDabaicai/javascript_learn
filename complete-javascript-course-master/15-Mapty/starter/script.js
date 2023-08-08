c9db8cb5c327a72a5c1c095c252a16661f8a49a3
DESKTOP-KR3UD5D\NfryCreator <54572905@qq.com>
2023-08-08 14:11:05 +0800

    地图图标设置

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -11,26 +11,43 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if(navigator.geolocation)
+if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition(
         function (position) {
-            const {latitude,longitude} = position.coords
-            console.log(latitude,longitude)
+            const {latitude, longitude} = position.coords
+            console.log(latitude, longitude)
             console.log(`https://www.google.com/maps/@${latitude},${longitude},11z?entry=ttu`)
 
-            const coords = [latitude,longitude]
+            const coords = [latitude, longitude]
 
-            const map = L.map('map').setView(coords,13);
+            const map = L.map('map').setView(coords, 13);
 
-            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',{
+            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
                 attribution:
-                '&copy;<a href="https://www.baidu.com"></a>'
+                    '&copy;<a href="https://www.baidu.com"></a>'
             }).addTo(map)
 
-            L.marker(coords)
-                .addTo(map)
-                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-                .openPopup();
+            map.on('click', function (mapEvent) {
+                console.log(mapEvent)
+                const {lat, lng} = mapEvent.latlng
+
+                L.marker([lat, lng])
+                    .addTo(map)
+                    .bindPopup(
+                        L.popup({
+                            maxWidth:250,
+                            minWidth:100,
+                            autoClose:false,
+                            closeOnClick:false,
+                            className:'running-popup'
+                        })
+                    )
+                    .setPopupContent('Workout')
+                    .openPopup();
+
+            })
+
+
 
         },
         function () {
