8deae662c01a3c020da7a52dcd59f0f700c0151b
DESKTOP-KR3UD5D\NfryCreator <54572905@qq.com>
2023-08-09 10:06:03 +0800

    软件结构面向对象化

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -11,67 +11,84 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map,mapEvent;
+let map, mapEvent;
+
+class App {
+    #map
+    #mapEvent
+
+    constructor() {
+        this._getPosition()
+        form.addEventListener('submit', this._newWorkout.bind(this))
+        inputType.addEventListener('change', this._toggleElevationField.bind(this))
+    }
+
+    _getPosition() {
+        if (navigator.geolocation)
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this),
+                function () {
+                    alert(`Could not get your position`)
+                }
+            )
+    }
+
+    _loadMap(position) {
+        const {latitude, longitude} = position.coords
+        console.log(latitude, longitude)
+        console.log(`https://www.google.com/maps/@${latitude},${longitude},11z?entry=ttu`)
+
+        const coords = [latitude, longitude]
+
+        this.#map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution:
+                '&copy;<a href="https://www.baidu.com"></a>'
+        }).addTo(this.#map)
+
+        this.#map.on('click', this._showForm.bind(this))
+    }
+
+    _showForm(mapE) {
+        this.#mapEvent = mapE
+        form.classList.remove('hidden')
+        inputDistance.focus()
+    }
+
+    _toggleElevationField() {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+    }
+
+    _newWorkout(e) {
+        e.preventDefault()
 
 
-if (navigator.geolocation)
-    navigator.geolocation.getCurrentPosition(
-        function (position) {
-            const {latitude, longitude} = position.coords
-            console.log(latitude, longitude)
-            console.log(`https://www.google.com/maps/@${latitude},${longitude},11z?entry=ttu`)
-
-            const coords = [latitude, longitude]
-
-            map = L.map('map').setView(coords, 13);
-
-            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-                attribution:
-                    '&copy;<a href="https://www.baidu.com"></a>'
-            }).addTo(map)
-
-            map.on('click', function (mapE) {
-                mapEvent = mapE
-                form.classList.remove('hidden')
-                inputDistance.focus()
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
 
 
+        console.log(mapEvent)
+        const {lat, lng} = this.#mapEvent.latlng
 
+        L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(
+                L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'running-popup'
+                })
+            )
+            .setPopupContent('Workout')
+            .openPopup();
+    }
 
-            })
-        },
-        function () {
-            alert(`Could not get your position`)
-        }
-    )
+}
 
-inputType.addEventListener('change',function () {
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
-})
-form.addEventListener('submit',function(e){
-    e.preventDefault()
+const app = new App()
 
-    inputDistance.value = inputDuration.value = inputCadence.value =inputElevation.value = ''
-
-
-    console.log(mapEvent)
-    const {lat, lng} = mapEvent.latlng
-
-    L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-            L.popup({
-                maxWidth:250,
-                minWidth:100,
-                autoClose:false,
-                closeOnClick:false,
-                className:'running-popup'
-            })
-        )
-        .setPopupContent('Workout')
-        .openPopup();
-})
 
 
 
