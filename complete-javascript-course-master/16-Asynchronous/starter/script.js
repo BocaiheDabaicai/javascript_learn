f05cf8dcf035b95644ce4f802a9a04a26b453253
DESKTOP-KR3UD5D\NfryCreator <54572905@qq.com>
2023-08-14 17:02:25 +0800

    Fetch、回调地狱、Promise

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -5,16 +5,9 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountryData = function(country) {
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v3.1/name/${country}`)
-    request.send()
-    request.addEventListener('load', function () {
-        // console.log(this.responseText)
-        const [data] = JSON.parse(this.responseText)
-        console.log(data)
 
-        const html = `
+const renderCountry = function (data) {
+    const html = `
         <article class="country">
           <img class="country__img" src="${data.flags.png}" />
           <div class="country__data">
@@ -27,12 +20,81 @@ const getCountryData = function(country) {
         </article>
     `
 
-        countriesContainer.insertAdjacentHTML('beforeend', html);
-        countriesContainer.style.opacity = 1
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1
+}
+
+const getCountryAndNeighbourData = function(country) {
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`)
+    request.send()
+    request.addEventListener('load', function () {
+        // console.log(this.responseText)
+        const [data] = JSON.parse(this.responseText)
+        console.log(data)
+        renderCountry(data)
+
     })
 }
 
-getCountryData('China')
+/* API请求
+getCountryAndNeighbourData('China')
 // getCountryData('America')
 // getCountryData('Canada')
-getCountryData('Germany')
+getCountryAndNeighbourData('Germany')
+*/
+
+/* 回调地狱
+setTimeout(()=>{
+    console.log('第1秒触发')
+    setTimeout(()=>{
+        console.log('第2秒触发')
+        setTimeout(()=>{
+            console.log('第3秒触发')
+            setTimeout(()=>{
+                console.log('第4秒触发')
+            },1000)
+        },1000)
+    },1000)
+},1000)
+*/
+
+/* Fetch函数
+const data = fetch(`https://restcountries.com/v3.1/name/china`)
+console.log(data)
+*/
+
+const getCountryData = function(country){
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+        .then(function(response){
+        console.log(response)
+        return response.json()
+    })
+        .then(function (data) {
+            console.log(data)
+        })
+}
+const getCountryDataS = function(country){
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+        .then((response)=> response.json())
+        .then( (data)=> renderCountry(data[0]))
+}
+// getCountryData('china')
+getCountryDataS('china')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
